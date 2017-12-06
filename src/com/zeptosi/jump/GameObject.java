e08ba5df0aaab15ab1730e395c58a21eb435dea8
Silas Müller <news@silas2000.de>
2017-12-06 12:52:37 +0100

    changed package name

---
+++ src/com/zeptosi/jump/GameObject.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zeptosi.jump;
+
+import javafx.scene.shape.Shape;
+
+/**
+ *
+ * @author Silas Müller
+ */
+public abstract class GameObject {
+    protected int x, y;
+    protected int velX, velY;
+    protected ID id;
+    
+    public GameObject(int pX, int pY, ID pID) {
+        this.x = pX;
+        this.y = pY;
+        this.id = pID;
+    }
+    
+    public abstract Shape initRender();
+    public abstract void tick();
+    public abstract void updateRender();
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getVelX() {
+        return velX;
+    }
+
+    public void setVelX(int velX) {
+        this.velX = velX;
+    }
+
+    public int getVelY() {
+        return velY;
+    }
+
+    public void setVelY(int velY) {
+        this.velY = velY;
+    }
+    
+    public ID getID() {
+        return id;
+    }
+    
+    
+    
+}
---
+++ src/com/zeptosi/jump/Handler.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zeptosi.jump;
+
+import java.util.LinkedList;
+import javafx.scene.shape.Shape;
+
+/**
+ *
+ * @author Silas Müller
+ */
+public class Handler {
+    private LinkedList<GameObject> gameObject;
+    
+    public Handler() {
+        gameObject = new LinkedList();
+    }
+    
+    public LinkedList<Shape> initRender() {
+        LinkedList<Shape> shape = new LinkedList<Shape>();
+        for(int i = 0; i < gameObject.size(); i++) {
+            shape.add(gameObject.get(i).initRender());
+        }
+        return shape;
+    }
+        
+    public void updateRender() {
+        for(int i = 0; i < gameObject.size(); i++) {
+            gameObject.get(i).updateRender();
+        }
+    }
+    
+    public void tick() {
+        for(int i = 0; i < gameObject.size(); i++) {
+            gameObject.get(i).tick();
+        }
+    }
+    
+    public void add(GameObject pGo) {
+        gameObject.add(pGo);
+    }
+    
+    public LinkedList<GameObject> getGameObjects() {
+        return gameObject;
+    }
+}
